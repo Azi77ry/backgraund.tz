79b4f57542474299ba900c8adb49be3804d82c7f
Your Name <you@example.com>
2025-04-20 13:33:54 +0300

    greate

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Free-tier optimized build
-pip install --no-cache-dir -r requirements.txt
-
-# Clean up to save space
-rm -rf ~/.cache/pip
-find /usr -name "__pycache__" -exec rm -rf {} +
